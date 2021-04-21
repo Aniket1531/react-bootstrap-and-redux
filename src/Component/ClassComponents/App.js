111b601144068a3d9ef76ed2cad3db65eb3fa565
Aniket Gupta <agupta@jssoftwares.com>
2021-04-21 14:05:40 +0530

    mapStateToProps update02

--- src/Component/ClassComponents/App.js
+++ src/Component/ClassComponents/App.js
@@ -1,14 +1,15 @@
 import React,{Component} from 'react';
 import FormC from "./FormC"
-
+import {connect} from 'react-redux'
+import List from './List'
 
 class App extends Component {
   render(){
      return (
     
       <section>
-        <FormC />
-        
+        <FormC isModalOpen={this.props.isModalOpen} addPerson={this.props.addPerson} noValue={this.props.noValue} />
+        <List people ={this.props.people}  deletePerson={this.props.deletePerson}   />
       </section>
     
     )
@@ -16,4 +17,22 @@ class App extends Component {
 
 }
 
-export default App
+const mapStateToProps = (state) =>{
+
+  return {
+      isModalOpen : state.isModalOpen,                
+      people : state.people
+  }
+}
+
+const mapDispatchToProps = (dispatch) =>{
+
+  return {
+    addPerson : (newItem)=> dispatch({type:'ADD_ITEM',payload:newItem}),
+    noValue : ()=> dispatch({type:'NO_VALUE'}),
+    deletePerson : (person)=>{dispatch({type:"REMOVE_ITEM",payload:person})}
+  }
+
+}
+
+export default connect(mapStateToProps,mapDispatchToProps)(App)
--- src/Component/ClassComponents/FormC.js
+++ src/Component/ClassComponents/FormC.js
@@ -1,9 +1,9 @@
 import React, { Component } from 'react'
-import {connect} from 'react-redux'
+
 import { Form,Button } from 'react-bootstrap';
 // import {useSelector,useDispatch} from "react-redux"
 import Modal from "./Modal"
-import List from './List'
+
 
 class FormC extends Component {
     
@@ -19,8 +19,7 @@ class FormC extends Component {
     handleAdd = (e) =>{
 
     e.preventDefault()
-    console.log("state",this.state)
-    console.log("prop",this.props)
+    console.log("this.name",this.name)
     const name = this.state.name
     if(name){
       const  newItem = {id : new Date().getTime().toString(),name}
@@ -49,29 +48,12 @@ class FormC extends Component {
         Add People
         </Button>
         </Form>
-        <List people ={this.props.people}  deletePerson={this.props.deletePerson}   />
+        
         </div>
       </div>
     )
   }
 }
-const mapStateToProps = (state) =>{
 
-  return {
-      isModalOpen : state.isModalOpen,                
-      people : state.people
-  }
-}
 
-const mapDispatchToProps = (dispatch) =>{
-
-  return {
-    addPerson : (newItem)=> dispatch({type:'ADD_ITEM',payload:newItem}),
-    noValue : ()=> dispatch({type:'NO_VALUE'}),
-    deletePerson : (person)=>{dispatch({type:"REMOVE_ITEM",payload:person})}
-  }
-
-}
-
-export default connect(mapStateToProps,mapDispatchToProps)(FormC)
-
+export default FormC
